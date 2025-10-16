79e2e0b72f689e6b8f874df85774614a85f42fed
Ekrem Ersoz <112519173+ersozekrem@users.noreply.github.com>
2025-10-16 13:53:04 -0700

    Use entire dataset for training and testing
    Changed train_test_split to use entire dataset for training and testing.

--- train_model.py
+++ train_model.py
@@ -18,7 +18,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = X, X, y, y
 
 # Train model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
